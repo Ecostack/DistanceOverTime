1d15987082f236dc6e58fbf16fc05c7516cb8017
Sebastian Scheibe <sebastian@ecostack.nl>
2024-03-21 18:59:46 +0800

    Initial Commit

---
+++ DistanceOverTime/DistanceOverTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DistanceOverTimeApp.swift
+//  DistanceOverTime
+//
+//  Created by Sebastian Scheibe on 21/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DistanceOverTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
